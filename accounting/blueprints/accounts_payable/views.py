b00f086d9fdb94ade374119a206daa0c83e5126b
alvin-c-cruz <alvinccruz12@gmail.com>
2022-07-05 21:12:34 +0800

    Modified /accounts_payable/view.py /add

--- accounting/blueprints/accounts_payable/views.py
+++ accounting/blueprints/accounts_payable/views.py
@@ -108,7 +108,7 @@ def add():
                 db.session.add(new_data)
                 db.session.commit()
                 flash(f"Added {new_data}", category="success")
-                return redirect(url_for("accounts_payable.home", page=1))
+                return redirect(url_for("accounts_payable.add"))
             else:
                 total_debit = "{:,.2f}".format(total_debit)
                 total_credit = "{:,.2f}".format(total_credit)
